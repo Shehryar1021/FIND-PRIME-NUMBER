ddfd91f82e44d7bbd75aa9f2f37665ecec855a43
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-24 16:09:42 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+lower = int(input("ENTER A LOWER RANGE :"))
+upper = int(input("ENTER A UPPER RANGE :"))
+print("PRIME NUMBERS BETWEEN", lower, "and", upper, "are:")
+for num in range(lower,upper + 1):
+   if num > 1:
+     for i in range(2,num):
+        if(num % i) ==0:
+           break
+     else:
+       print(num)      
